f6ad7c5bb287492742e77af8ff4c251d0e282b53
0ceanlight <imahacubak@gmail.com>
2025-11-22 10:15:19 +0100

    Initial Commit

---
+++ SnapFocus/SnapFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapFocusApp.swift
+//  SnapFocus
+//
+//  Created by Francesca Frederick on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
